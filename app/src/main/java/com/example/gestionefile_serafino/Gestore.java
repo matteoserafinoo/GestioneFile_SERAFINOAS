31e40dc0192ded91ebea52851332b85b6cba0bd1
CaporaliGiorgio <giorgio.caporali.1@gmail.com>
2022-02-10 08:50:27 +0100

    aggiunto metodo per scrittura testo

--- app/src/main/java/com/example/gestionefile_serafino/Gestore.java
+++ app/src/main/java/com/example/gestionefile_serafino/Gestore.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class Gestore {
     String nomefile;
@@ -37,8 +40,21 @@ public class Gestore {
 
     }
 
-    public void ScriviFile(String nomefile, String testo)
+    public String ScriviFile(String nomefile, Context c)
     {
-
+        FileOutputStream file0;
+        String esito="";
+        String testodascrivere = "QUesto è il testo del file";
+        try {
+            file0 = c.openFileOutput(nomefile, Context.MODE_PRIVATE);
+            file0.write(testodascrivere.getBytes());
+            file0.close();
+            esito = "Tutto bene";
+        } catch (FileNotFoundException e) {
+            esito = "Attenzione errore apertura file";
+        } catch (IOException e) {
+            esito= "Errore nella scrittura";
+        }
+        return esito;
     }
 }
